e459adcd24b8ddc4dba01a5c66009aab7b658513
dm <muk_dinara@mail.ru>
2019-02-26 16:43:27 +0600

    Добавьте файлы проекта.

---
+++ hm_interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Student s = new Student("Ivan", "Ivanov", 7);
+            for (int i = 0; i < s.Grades.Length; i++)
+            {
+                s.Grades[i] = rnd.Next(12);                
+            }
+            Console.WriteLine("Average grades: {0}", s.GetAvgGrade());
+
+            
+        }
+    }
+}
---
+++ hm_interfaces/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm_interfaces
+{
+    public class Student : IStudent
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int[] Grades { get; set; }
+
+        public Student(string name, string fullName, int grades)
+        {
+            this.Name = name;
+            this.FullName = fullName;
+            Grades = new int[grades];
+        }
+        public double GetAvgGrade()
+        {
+            int sum = 0;
+            foreach (int i in Grades)
+            {
+                sum += i;
+            }
+            return Convert.ToDouble(sum/Grades.Length);
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
